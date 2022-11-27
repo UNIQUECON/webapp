f39718cd5e942c372f09e064a0ccf3b64f5e7b30
UNIQUECON <116038972+UNIQUECON@users.noreply.github.com>
2022-11-27 12:50:55 +0000

    fix(): fixed

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -32,6 +32,7 @@ const ProductList = () => {
             queryId,
         }
         try {
+            alert(data)
             fetch('http://188.93.210.188:8300/web-data', {
                 method: 'POST',
                 headers: {
