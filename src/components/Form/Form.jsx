01fde7bbec94c680620eaeab0ce9ceb824b867e8
UNIQUECON <116038972+UNIQUECON@users.noreply.github.com>
2022-12-10 14:03:07 +0000

    feat(): some changes for test

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -17,12 +17,12 @@ const Form = () => {
     //     tg.sendData(JSON.stringify(data));
     // }, [country, street, subject])
 
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
+    // useEffect(() => {
+    //     tg.onEvent('mainButtonClicked', onSendData)
+    //     return () => {
+    //         tg.offEvent('mainButtonClicked', onSendData)
+    //     }
+    // }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
