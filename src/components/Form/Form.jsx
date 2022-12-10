c66ff80a4800e56a542a87f73fb67c63c0f82e0f
UNIQUECON <116038972+UNIQUECON@users.noreply.github.com>
2022-12-10 14:19:51 +0000

    feat(): some changes for test

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,24 +5,34 @@ import {useTelegram} from "../../hooks/useTelegram";
 const Form = () => {
     const [country, setCountry] = useState('');
     const [street, setStreet] = useState('');
+    const [clicked, setClicked] = useState(false);
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
-    // const onSendData = useCallback(() => {
-    //     const data = {
-    //         country,
-    //         street,
-    //         subject
-    //     }
-    //     tg.sendData(JSON.stringify(data));
-    // }, [country, street, subject])
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [clicked])
+    
+        const sendClicked = () => {
+            const data = {
+                country,
+                street,
+                subject
+            }
+            tg.sendData(JSON.stringify(data));
+        }
 
-    // useEffect(() => {
-    //     tg.onEvent('mainButtonClicked', onSendData)
-    //     return () => {
-    //         tg.offEvent('mainButtonClicked', onSendData)
-    //     }
-    // }, [onSendData])
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [sendClicked])
 
     useEffect(() => {
         tg.MainButton.setParams({
@@ -50,15 +60,6 @@ const Form = () => {
         setSubject(e.target.value)
     }
 
-    const sendClicked = () => {
-        const data = {
-            country,
-            street,
-            subject
-        }
-        tg.sendData(JSON.stringify(data));
-    }
-
     return (
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
@@ -80,7 +81,7 @@ const Form = () => {
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
             </select>
-            <bitton onClick={sendClicked}>SEND</bitton>
+            <bitton onClick={() => setClicked(true)}>SEND</bitton>
         </div>
     );
 };
